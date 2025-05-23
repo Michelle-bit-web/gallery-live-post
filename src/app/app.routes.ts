d2261e537b466ed0bf7e105108a7fc6e4f17fa2f
Michelle <mpuschkarow@web.de>
2025-05-23 18:04:50 +0200

    routes generated for different contents

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PostListComponent } from './post-list/post-list.component';
+import { AuthComponent } from './auth/auth.component';
+import { PostEditComponent } from './post-edit/post-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'post-list', component: PostListComponent},
+    {path: 'auth', component: AuthComponent},
+    {path: 'post-add', component: PostEditComponent},
+];

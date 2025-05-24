c585d402a7de6b6c7af455292018f5877ebf071d
Michelle <mpuschkarow@web.de>
2025-05-24 11:12:30 +0200

    ngSubmit added to form for listening to subit button

--- src/app/post-edit/post-edit.component.ts
+++ src/app/post-edit/post-edit.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PostService } from '../post.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-edit',
   imports: [RouterModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './post-edit.component.html',
   styleUrl: './post-edit.component.scss'
 })
 export class PostEditComponent {
-  form?: FormGroup;
+  form!: FormGroup;
   constructor(private postService: PostService){};
 
   ngOnInit(): void {
@@ -20,5 +21,9 @@ export class PostEditComponent {
       description: new FormControl(null),
       imagePath: new FormControl(null),
     })
+  }
+
+  onSubmit(){
+    console.log('onSubit() is called')
   }
 }
